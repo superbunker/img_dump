7b68f64f2b95dd51400f33b32b799d7c375596b5
Jason Brown <superbunker@gmail.com>
2024-12-28 22:50:20 -0800

    update

--- test.js
+++ test.js
@@ -1,46 +1,11 @@
-// function sumAllNumbers() {
-//     // Numbers from stockings and mittens
-//     const numbers = [
-//         1, 4, 6, // First row of stockings
-//         5, 8,    // Second row of mittens
-//         2, 3, 6, 8, // Third row of stockings
-//         4, 5, 8   // Fourth row of mittens
-//     ];
+// The counted squares
+let yellowSquares = 14; 
+let greenSquaresIn8thRow = 4; 
 
-//     // Calculating the sum
-//     const totalSum = numbers.reduce((acc, num) => acc + num, 0);
-//     return totalSum;
-// }
-
-// // Output the result
-// console.log(sumAllNumbers()); // Output: 60
-
-// function sumNumbersAndCountObjects(numbers, totalObjects) {
-//     const numberSum = numbers.reduce((acc, num) => acc + num, 0);
-//     console.log(numberSum);
-//     return numberSum + totalObjects;
-// }
-
-// // Numbers from the image
-// const numbersFromImage = [1, 4, 6, 5, 8, 2, 6, 8, 4, 5, 8];
-
-// // Total objects (16 stockings + 12 mittens)
-// const totalObjects = 16 + 12;
-
-// // Final result
-// console.log(sumNumbersAndCountObjects(numbersFromImage, totalObjects)); // Output: 101
-
-// Function to sum numbers and add the number of objects
-function sumNumbersAndCountObjects(numbers, totalObjects) {
-    const numberSum = numbers.reduce((acc, num) => acc + num, 0);
-    return numberSum + totalObjects;
+// Function to multiply the counts
+function multiplyYellowByGreen(yellow, green) {
+    return yellow * green;
 }
 
-// Numbers from the image
-const numbersFromImage = [1, 4, 6, 5, 8, 2, 3, 6, 8, 4, 5, 8];
-
-// Total objects (16 stockings + 12 mittens)
-const totalObjects = 16 + 12;
-
-// Final result
-console.log(sumNumbersAndCountObjects(numbersFromImage, totalObjects)); // Output: 85
+// Output the result
+console.log(multiplyYellowByGreen(yellowSquares, greenSquaresIn8thRow)); // returns 18
